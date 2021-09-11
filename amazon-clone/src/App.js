90faac11834a060443ff62bc13a4978d7211c2e5
mohdazeem0 <mohdazm007@gmail.com>
2021-09-11 17:50:28 +0530

    Nav bar added

---
+++ amazon-clone/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/checkout">
+            <h1>checkout</h1>
+          </Route>
+          <Route path="/login">
+            <h1>Login Page</h1>
+          </Route>
+          <Route path="/">
+            <h1>HOME PAGE</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ amazon-clone/src/Header/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function componentName() {
+  return (
+    <nav className="header">
+      {/* logo  */}
+      <img
+        className="header__logo"
+        src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        alt="amazonlogo"
+      />
+      {/* search box  */}
+      {/* three anchor links  */}
+      {/* basket icon with items number  */}
+    </nav>
+  );
+}
+
+export default componentName;
