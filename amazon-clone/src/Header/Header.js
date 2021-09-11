5a309172f5bacf1c4d0e6bf186da30b0eada79b7
mohdazeem0 <mohdazm007@gmail.com>
2021-09-11 18:21:04 +0530

    nav bar and logo added with styling

--- amazon-clone/src/Header/Header.js
+++ amazon-clone/src/Header/Header.js
@@ -1,15 +1,21 @@
 import React from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
 
 function componentName() {
   return (
     <nav className="header">
       {/* logo  */}
-      <img
-        className="header__logo"
-        src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
-        alt="amazonlogo"
-      />
+      <Link to="/">
+        <img
+          className="header__logo"
+          src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+          alt="amazonlogo"
+        />{" "}
+      </Link>
+
       {/* search box  */}
+      <input type="text" className="header__searchInput" />
       {/* three anchor links  */}
       {/* basket icon with items number  */}
     </nav>
